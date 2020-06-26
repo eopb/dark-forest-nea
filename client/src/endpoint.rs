7ca2fabc9b8a8e5d0337138872f4b4f90eadc7b8
Ethan Brierley <ethanboxx@gmail.com>
2020-06-26 16:45:23 +0100

    Allow same-origin credentials

--- client/src/endpoint.rs
+++ client/src/endpoint.rs
@@ -1,6 +1,9 @@
-use async_trait::async_trait;
-use glue::Endpoint as _;
-use seed::{browser::fetch::FetchError, prelude::*};
+use {
+    async_trait::async_trait,
+    glue::Endpoint as _,
+    seed::{browser::fetch::FetchError, prelude::*},
+    web_sys::RequestCredentials::SameOrigin,
+};
 #[async_trait(?Send)]
 pub trait Endpoint: glue::Endpoint {
     async fn fetch() -> Result<Self, FetchError>;
@@ -9,6 +12,12 @@ pub trait Endpoint: glue::Endpoint {
 #[async_trait(?Send)]
 impl Endpoint for glue::Hello {
     async fn fetch() -> Result<Self, FetchError> {
-        Request::new(Self::PATH).fetch().await?.json().await
+        Request::new(Self::PATH)
+            //TODO Maybe this is default so lets try removing it when we need it.
+            .credentials(SameOrigin)
+            .fetch()
+            .await?
+            .json()
+            .await
     }
 }
