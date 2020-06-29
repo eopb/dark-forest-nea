966c6dd5100bb0dbb98020bcdb5de8f6270552a3
Ethan Brierley <ethanboxx@gmail.com>
2020-06-29 14:59:06 +0100

    remove unneeded `credentials(SameOrigin)`
    as it is default in most modern browsers

--- client/src/endpoint.rs
+++ client/src/endpoint.rs
@@ -2,18 +2,11 @@ use {
     async_trait::async_trait,
     seed::{browser::fetch::FetchError, prelude::*},
     serde::{Deserialize, Serialize},
-    web_sys::RequestCredentials::SameOrigin,
 };
 #[async_trait(?Send)]
 pub trait Endpoint: 'static + glue::Endpoint + Serialize + for<'a> Deserialize<'a> {
     async fn fetch() -> Result<Self, FetchError> {
-        Request::new(Self::PATH)
-            // TODO Maybe this is default so lets try removing it when we need it.
-            .credentials(SameOrigin)
-            .fetch()
-            .await?
-            .json()
-            .await
+        Request::new(Self::PATH).fetch().await?.json().await
     }
 }
 
