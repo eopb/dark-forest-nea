bf244d1b297be0b121ebf2d638430fe235ae5bc2
Ethan Brierley <ethanboxx@gmail.com>
2020-06-29 18:09:26 +0100

    Use templating rather than HTML

--- server/src/routes.rs
+++ server/src/routes.rs
@@ -1,5 +1,7 @@
 //! Application endpoints.
 
+use maud::{html, PreEscaped, DOCTYPE};
+
 use super::State;
 
 use {
@@ -11,7 +13,46 @@ pub async fn index(_: Request<State>) -> tide::Result<impl Into<Response>> {
     Ok({
         let mut res = Response::new(StatusCode::Ok);
         res.set_content_type(mime::HTML);
-        res.set_body(include_str!("../../client/index.html"));
+        res.set_body(
+            html! {
+                (DOCTYPE)
+                head {
+                    meta charset="utf-8";
+                    meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1, shrink-to-fit=no";
+                    style {
+                        (PreEscaped(r#"
+                            @font-face {
+                                font-family: bitlimt;
+                                src: url("fonts/8-bit-limit/8bitlim.ttf");
+                                font-weight: bold;
+                            }
+                            @font-face {
+                                font-family: adobedia;
+                                src: url("fonts/levi-adobe-dia/adobedia.ttf");
+                                font-weight: bold;
+                            }
+                            @font-face {
+                                font-family: prstart;
+                                src: url("fonts/press-start/prstart.ttf");
+                                font-weight: bold;
+                            }
+                        "#))
+                    }
+                }
+                body {
+                    section id="app" {}
+                    script type="module" {
+                        (PreEscaped(r#"
+                            import init from "/pkg/package.js";
+                            init("/pkg/package_bg.wasm");
+                        "#))
+                    }
+                }
+            }
+            .into_string(),
+        );
         res
     })
 }
