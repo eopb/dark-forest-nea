effd2f61af740b64e3cf894dae71a07c18b2e32e
Ethan Brierley <ethanboxx@gmail.com>
2020-07-21 15:10:39 +0100

    Fill in all the detail I want at this point

--- client/src/ui/router/editor.rs
+++ client/src/ui/router/editor.rs
@@ -1,4 +1,8 @@
-use crate::{state, ui, updates};
+use crate::{
+    state,
+    ui::{self, view::View},
+    updates,
+};
 use seed::{prelude::*, *};
 use seed_style::{em, pc, px, *};
 
@@ -22,6 +26,7 @@ impl From<Msg> for updates::Msg {
     }
 }
 
+#[allow(clippy::too_many_lines)]
 pub fn view(model: &state::Model) -> Node<updates::Msg> {
     log!(model.route_data.editor);
     match &model.route_data.editor {
@@ -47,8 +52,107 @@ pub fn view(model: &state::Model) -> Node<updates::Msg> {
             ui::form::InputBuilder::text_area()
                 .value(&project.description)
                 .label("Description")
-                .view(model, |x| Some(Msg::DescriptionChanged(x).into()))
+                .view(model, |x| Some(Msg::DescriptionChanged(x).into())),
+            label![
+                s().margin("0")
+                    .margin_bottom(px(-15))
+                    .width(px(600))
+                    .text_align_left()
+                    .font_size(em(2.9))
+                    .color(model.theme.text()),
+                "Chapters"
+            ],
+            ui::Bordered::new(
+                vec![project
+                    .chapters
+                    .iter()
+                    .map(|(key, chapter)| div![
+                        s().padding_left(px(8)).padding_right(px(8)),
+                        vec![div![
+                            s().display_grid()
+                                .grid_template_columns("70px auto")
+                                .grid_gap(px(8))
+                                .width(pc(100)),
+                            ui::form::InputBuilder::text()
+                                .value(&key)
+                                .width(pc(100))
+                                .view(model, |_| None),
+                            ui::form::InputBuilder::text()
+                                .value(&chapter.heading)
+                                .width(pc(100))
+                                .view(model, |_| None),
+                        ]],
+                        ui::form::InputBuilder::text_area()
+                            .value(&chapter.body)
+                            .width(pc(100))
+                            .view(model, |x| None),
+                        vec![
+                            vec![label![
+                                s().margin("0")
+                                    .margin_bottom(px(-15))
+                                    .width(px(600))
+                                    .text_align_left()
+                                    .font_size(em(2.9))
+                                    .color(model.theme.text()),
+                                "Decisions"
+                            ]],
+                            ui::Bordered::new(
+                                chapter
+                                    .decisions
+                                    .iter()
+                                    .map(|decision| div![
+                                        s().padding_left(px(8)).padding_right(px(8)),
+                                        vec![div![
+                                            s().display_grid()
+                                                .grid_template_columns("150px auto")
+                                                .grid_gap(px(8))
+                                                .width(pc(100)),
+                                            if let Some(shared::data::Link::Chapter(goes_to)) =
+                                                decision.goes_to
+                                            {
+                                                nodes![
+                                                    p![
+                                                        s().font_size(em(2.9))
+                                                            .margin("0")
+                                                            .margin_bottom("auto")
+                                                            .margin_top("auto"),
+                                                        "goes_to"
+                                                    ],
+                                                    ui::form::InputBuilder::text()
+                                                        .value(&goes_to)
+                                                        .width(pc(100))
+                                                        .view(model, |_| None),
+                                                ]
+                                            } else {
+                                                vec![empty()]
+                                            }
+                                        ]],
+                                        ui::form::InputBuilder::submit()
+                                            .value(&decision.body)
+                                            .width(pc(100))
+                                            .view(model, |x| None),
+                                    ])
+                                    .collect::<Vec<Node<updates::Msg>>>()
+                            )
+                            .inner(s().width(pc(100)))
+                            .view(model),
+                            ui::form::InputBuilder::submit()
+                                .value("Add decision")
+                                .view(model, |_| None)
+                        ]
+                    ])
+                    .collect::<Vec<Node<updates::Msg>>>(),]
+                .into_iter()
+                .flatten()
+                .collect::<Vec<Node<updates::Msg>>>()
+            )
+            .inner(s().width(px(600)))
+            .view(model),
+            ui::form::InputBuilder::submit()
+                .value("Add chapter")
+                .view(model, |_| None)
         ]],
         Err(error) => div!["some error"],
     }
 }
+// TODO use type alias for `Node<updates::Msg>>`
